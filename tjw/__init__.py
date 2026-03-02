32ca7d24bcc6d0d9ac4ca111fa30d8b90fbfff6a
duduzixun <duduzixun@163.com>
2026-03-02 22:43:16 +0800

    第一次上传TJW库源码

---
+++ tjw/__init__.py
@@ -0,0 +1,5 @@
+from tjw.core import *
+
+if __name__ == '__main__':
+    result = tjw.helloworld()
+    print(f"结果：{result}")
---
+++ tjw/core.py
@@ -0,0 +1,6 @@
+class tjw类:
+    def helloworld(self, name: str = "tjw"):
+        return f"hello {name}!"
+
+
+tjw = tjw类()
